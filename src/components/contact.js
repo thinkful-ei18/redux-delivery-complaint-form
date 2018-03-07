0bf961bfc5ba7f280cf22b7a0e2d7acf82e496b1
Adrian <adriantoddross@gmail.com>
2018-03-07 12:00:06 -0500

    test css

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './contact.css';
+import './contact.css'
 export default class Contact extends React.Component {
   render() {
     return (
@@ -8,21 +8,24 @@ export default class Contact extends React.Component {
           <h1>Report a problem with your delivery</h1>
         </header>
         <main>
-          <form>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            console.log(e.target.value);
+          }}>
             <label> Tracking Number
               <input type="text"/>
             </label>
             <label> What is your issue?
               <select name="issue">
-              <option value="">My delivery hasn't arrived</option>
-              <option value="">The wrong item was delivered</option>
-              <option value="">Part of my order was missing</option>
-              <option value="">Some of my order arrived damaged</option>
-              <option value="">Other (give more details below)</option>
+              <option value="missing">My delivery hasn't arrived</option>
+              <option value="wrong item">The wrong item was delivered</option>
+              <option value="incomplete">Part of my order was missing</option>
+              <option value="damaged">Some of my order arrived damaged</option>
+              <option value="other">Other (give more details below)</option>
               </select>
             </label>
             <label htmlFor=""> Give more details (optional)
-              <textarea name="" id="" cols="30" rows="5"></textarea>
+              <textarea name="details" id="details" cols="30" rows="5"></textarea>
             </label>
             <button type="submit">Submit</button>
           </form>
