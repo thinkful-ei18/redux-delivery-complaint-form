647035b0cdb615159236c77d83a71ce29da08704
Adrian <adriantoddross@gmail.com>
2018-03-07 14:27:35 -0500

    add all validators

---
+++ src/validators.js
@@ -0,0 +1,12 @@
+export const required = value => (value ? undefined : 'Required');
+
+export const nonEmpty = value =>
+    value.trim() !== '' ? undefined : 'Cannot be empty';
+
+export const maxChars = value =>
+    /\d{5}/.test(value) ? undefined : 'Tracking number must be five numbers';
+
+export const numbersOnly = value =>
+    /.*?(\d)[^\d]*/g.test(value) ? undefined : 'Tracking number can only consist of numbers';
+
+// export const trackingLength = value => value.length === 5 && /\d{5}/.test(value) ? undefined : 'Tracking number must be 5 numbers';
