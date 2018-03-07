1fa81dc8936191d00b4418ad6c7f1588817e510b
Adrian <adriantoddross@gmail.com>
2018-03-07 11:34:37 -0500

    add eslint exception and stub main component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Contact from './components/contact';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Contact />, document.getElementById('root'));
 registerServiceWorker();
